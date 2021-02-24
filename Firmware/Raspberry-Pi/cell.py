d4dbdf416f9e5f0fb11a1417fb9c4d9346589bab
Paramintr Tienpasertkij <paramintrt94@gmail.com>
2021-02-24 10:16:44 -0500

    Implemeneted better strategy to ignore ambient light reading and cleaned up code

--- Firmware/Raspberry-Pi/cell.py
+++ Firmware/Raspberry-Pi/cell.py
@@ -1,96 +1,124 @@
 # Importing libraries used to program this script
-from gpiozero import RGBLED
 from colorzero import Color
-from time import sleep
 from time import time
 import math
 
 class Cell:
-    colorSensitivity = 1    # set color sensitivity (higher number allows higher variance in color reading)
-    certaintyLevel   = 4    # set higher value to ignore more ambient lighting when no piece is placed
-    immuneDuration   = 6    # set to how many seconds before immune reset
-    expValue         = 0.1  # set how aggressive to fade color, lower value keeps green on longer
-    kValue           = math.log(1/expValue)
-    
+    color_sensitivity = 2  # set color sensitivity (higher number allows higher variance in color reading)
+    certainty_level = 5  # set higher value to ignore more ambient lighting when no piece is placed
+    immune_duration = 6  # set to how many seconds before immune reset
+    exp_value = 0.1  # set how aggressive to fade color, lower value keeps green on longer
+    k_value = math.log(1 / exp_value)
+    red_limits = [range(30 - color_sensitivity, 43 + color_sensitivity),
+                  range(8 - color_sensitivity, 11 + color_sensitivity),
+                  range(8 - color_sensitivity, 11 + color_sensitivity)]
+    green_limits = [range(0 - color_sensitivity, 5 + color_sensitivity),
+                    range(25 - color_sensitivity, 31 + color_sensitivity),
+                    range(17 - color_sensitivity, 29 + color_sensitivity)]
+    white_limits = [range(9 - color_sensitivity, 13 + color_sensitivity),
+                    range(18 - color_sensitivity, 22 + color_sensitivity),
+                    range(14 - color_sensitivity, 22 + color_sensitivity)]
+
     def __init__(self, led, idx):
         self.idx = idx
         self.status = "healthy"
         self.led = led
-        self.led.color = (0,0,0)
-        self.lastImmunized = -1
-        self.prevColor = ""
-        self.hysterisisCount = 1
-    #end of init
-        
-    def updateStatus(self, sensor, debug=False):
-        color = self.checkColor(sensor, debug)     #runs checkColor function to get color reading (returns: "Green", "Red", "White", or "Invalid")
+        self.led.color = (0, 0, 0)
+        self.last_immunized = -1
+        self.prev_color = ""
+        self.prev_color_readings = [[], [], []]  # [[list of prev red], [list of prev green], [list of prev blue]]
+        self.consistency_count = 0
+
+    # end of init
+
+    def update_status(self, sensor, debug_level=0):
+        # updates cell and LED color to respond to piece being placed on sensor
+        color = self.check_color(sensor, debug_level)
         if self.status == "healthy":
             None if self.led.is_lit else self.led.on()
             if color == "red":
-                print("Cell "+str(self.idx)+" is now infected.") if debug else None
+                print("Cell " + str(self.idx) + " is now infected.") if debug_level >= 1 else None
                 self.status = "infected"
-                self.led.color = (1,0,0)
+                self.led.color = (1, 0, 0)
             elif color == "green" or color == "white":
-                print("Cell "+str(self.idx)+" is now immunized.") if debug else None
+                print("Cell " + str(self.idx) + " is now immunized.") if debug_level >= 1 else None
                 self.status = "immune"
-                self.lastImmunized = time()
-                self.led.color = (0,1,0)
+                self.last_immunized = time()
+                self.led.color = (0, 1, 0)
         elif self.status == "infected":
             if color == "green" or color == "white":
-                print("Cell "+str(self.idx)+" is now immunized.") if debug else None
+                print("Cell " + str(self.idx) + " is now immunized.") if debug_level >= 1 else None
                 self.status = "immune"
-                self.lastImmunized = time()
-                self.led.color = (0,1,0)
+                self.last_immunized = time()
+                self.led.color = (0, 1, 0)
         elif self.status == "immune":
-            elapsedTime = time()-self.lastImmunized
-            elapsedTimePercent = elapsedTime/self.immuneDuration
-            if elapsedTimePercent >= 1:
-                print("Cell "+str(self.idx)+" immune duration is over. Reset to healthy.") if debug else None
+            elapsed_time = time() - self.last_immunized
+            elapsed_time_percent = elapsed_time / self.immune_duration
+            if elapsed_time_percent >= 1:
+                print("Cell " + str(
+                    self.idx) + " immune duration is over. Reset to healthy.") if debug_level >= 1 else None
                 self.status = "healthy"
-                self.led.color = (1,1,1)
+                self.led.color = (1, 1, 1)
             else:
-                self.led.color = (self.expValue*math.exp(self.kValue*elapsedTimePercent), 1, self.expValue*math.exp(self.kValue*elapsedTimePercent))
-                
-    def checkColor(self, sensor, debug):
-        # reads sensor color data and outputs color as string (red, green, white, invalid) might need tweaking to calibrate colors
-        sensorColorRGB = sensor.color_rgb_bytes
-                    
-        if ((30-self.colorSensitivity <= sensorColorRGB[0] <= 43+self.colorSensitivity) and (8-self.colorSensitivity <= sensorColorRGB[1] <= 11+self.colorSensitivity) and (8-self.colorSensitivity <= sensorColorRGB[2] <= 11+self.colorSensitivity)) : #checking for red piece
-            if self.prevColor == "red":
-                self.hysterisisCount += 1
-                if self.hysterisisCount >= self.certaintyLevel:
-                    print("S"+str(self.idx)+" detected virus piece") if self.hysterisisCount == self.certaintyLevel else None                        
-                    return "red"
-            else:
-                self.hysterisisCount = 0
-            self.prevColor = "red"
+                fade_level = self.exp_value * math.exp(self.k_value * elapsed_time_percent)
+                self.led.color = (fade_level, 1, fade_level)
 
-        elif ((sensorColorRGB[0] <= 5+self.colorSensitivity) and (25-self.colorSensitivity <= sensorColorRGB[1] <= 31+self.colorSensitivity) and (17-self.colorSensitivity <= sensorColorRGB[2] <= 29+self.colorSensitivity)): #checking for green piece
-            if self.prevColor == "green":
-                self.hysterisisCount += 1
-                if self.hysterisisCount >= self.certaintyLevel:
-                    print("S"+str(self.idx)+" detected deactivated virus vaccine") if self.hysterisisCount == self.certaintyLevel else None            
-                    return "green"
+    def is_consistent(self, rgb_color_reading):
+        for i in range(3):
+            self.prev_color_readings[i].append(rgb_color_reading[i])
+        if len(self.prev_color_readings[0]) < self.certainty_level + 1:
+            return False
+        else:
+            for i in range(3):
+                self.prev_color_readings[i].pop(0)
+            if (max(self.prev_color_readings[0]) - min(self.prev_color_readings[0])) <= self.color_sensitivity and (
+                    max(self.prev_color_readings[1]) - min(self.prev_color_readings[1])) <= self.color_sensitivity and (
+                    max(self.prev_color_readings[2]) - min(self.prev_color_readings[2])) <= self.color_sensitivity:
+                self.consistency_count += 1
+                return True
             else:
-                self.hysterisisCount = 0
-            self.prevColor = "green"            
-            
-        elif ((9-self.colorSensitivity <= sensorColorRGB [0] <= 13+self.colorSensitivity) and (18-self.colorSensitivity <= sensorColorRGB[1] <= 22+self.colorSensitivity) and (14-self.colorSensitivity <= sensorColorRGB[2] <= 22+self.colorSensitivity)): #checking for white piece
-            if self.prevColor == "white":
-                self.hysterisisCount += 1
-                if self.hysterisisCount >= self.certaintyLevel:
-                    print("S"+str(self.idx)+" detected mRNA messenger piece") if self.hysterisisCount == self.certaintyLevel else None
-                    return "white"
+                self.consistency_count = 0
+                return False
+
+    def check_color(self, sensor, debug_level):
+        # reads sensor color data and outputs color as string (red, green, white, invalid)
+        sensor_color_rgb = sensor.color_rgb_bytes
+        print("S" + str(self.idx) + " color reading:", sensor_color_rgb,
+              "\t[" + self.prev_color + "]") if debug_level >= 2 else None  # print rgb sensor reading in (r,g,b) values
+
+        if self.is_consistent(sensor_color_rgb):
+            # checking for red piece
+            if (sensor_color_rgb[0] in self.red_limits[0]) and (
+                    sensor_color_rgb[1] in self.red_limits[1]) and (
+                    sensor_color_rgb[2] in self.red_limits[2]):
+                print("S" + str(
+                    self.idx) + " detected virus piece") if self.consistency_count == self.certainty_level and debug_level >= 1 else None
+
+                self.prev_color = "red"
+                return "red"
+
+            # checking for green piece
+            elif (sensor_color_rgb[0] in self.green_limits[0]) and (
+                    sensor_color_rgb[1] in self.green_limits[1]) and (
+                    sensor_color_rgb[2] in self.green_limits[2]):
+                print("S" + str(
+                    self.idx) + " detected deactivated virus vaccine") if self.consistency_count == self.certainty_level and debug_level >= 1 else None
+                self.prev_color = "green"
+                return "green"
+
+            # checking for white piece
+            elif (sensor_color_rgb[0] in self.white_limits[0]) and (
+                    sensor_color_rgb[1] in self.white_limits[1]) and (
+                    sensor_color_rgb[2] in self.white_limits[2]):
+                print("S" + str(
+                    self.idx) + " detected mRNA messenger piece") if self.consistency_count == self.certainty_level and debug_level >= 1 else None
+                self.prev_color = "white"
+                return "white"
+
             else:
-                self.hysterisisCount = 0
-            self.prevColor = "white"            
+                self.prev_color = "invalid"
+                return "invalid"
 
         else:
-            self.hysterisisCount = 0 if self.prevColor != "invalid" else None
-            self.prevColor = "invalid"
+            self.prev_color = "invalid"
             return "invalid"
-        
-        print("S"+str(self.idx)+" color reading:",sensorColorRGB,"\t["+self.prevColor+"]") if debug else None            # this will print the rgb sensor color reading in (r,g,b) values
-            
-    def debugColor(self, sensor):
-        print("S"+str(self.idx)+" color reading:",sensor.color_rgb_bytes)
--- Firmware/Raspberry-Pi/vaccine-main.py
+++ Firmware/Raspberry-Pi/vaccine-main.py
@@ -1,66 +1,64 @@
 # Importing libraries used to program this script
 from gpiozero import RGBLED, Button
-from colorzero import Color
 from time import sleep
 from cell import Cell
-from signal import pause
-
-# Logger
-import logging
 
 # Initializing RPI board and I2C ports
 import board
 import busio
-import adafruit_tcs34725    # RGB Sensor Library
-import adafruit_tca9548a    # Multiplexer Library
+import adafruit_tcs34725  # RGB Sensor Library
+import adafruit_tca9548a  # Multiplexer Library
 
-i2c = busio.I2C(board.SCL, board.SDA)   # Initiate I2C object
-mpx = adafruit_tca9548a.TCA9548A(i2c)   # multiplexer object
+i2c = busio.I2C(board.SCL, board.SDA)  # Initiate I2C object
+mpx = adafruit_tca9548a.TCA9548A(i2c)  # multiplexer object
 
 # Specify which channels on the TCA9548A multiplexer are being used
-mpx_channels = [3,4,5]
-sensorArray = []
+mpx_channels = [3, 4, 5]
+sensor_array = []
 for mpx_channel in mpx_channels:
-    sensorArray.append(adafruit_tcs34725.TCS34725(mpx[mpx_channel]))
+    sensor_array.append(adafruit_tcs34725.TCS34725(mpx[mpx_channel]))
 
 # Specifying RGB Pins using GPIO#   # RGB PIN EQUIVALENT
-led1 = RGBLED(5,6,13)               # RGB LED1 PINS: RED PIN 29, GREEN PIN 31, BLUE PIN 33
-led2 = RGBLED(19,26,12)             # RGB LED2 PINS: RED PIN 35, GREEN PIN 37, BLUE PIN 32
-led3 = RGBLED(16,20,21)             # RGB LED3 PINS: RED PIN 36, GREEN PIN 38, BLUE PIN 40
-ledArray = [led1,led2,led3]
+led1 = RGBLED(5, 6, 13)  # RGB LED1 PINS: RED PIN 29, GREEN PIN 31, BLUE PIN 33
+led2 = RGBLED(19, 26, 12)  # RGB LED2 PINS: RED PIN 35, GREEN PIN 37, BLUE PIN 32
+led3 = RGBLED(16, 20, 21)  # RGB LED3 PINS: RED PIN 36, GREEN PIN 38, BLUE PIN 40
+ledArray = [led1, led2, led3]
 
 # Toggle Switch
 button = Button(4)
 
 # Create Array of "Cells"
 number_of_groups = 3
-cellArray = []
+cell_array = []
 for i in range(number_of_groups):
-    cellArray.append(Cell(ledArray[i], i))
+    cell_array.append(Cell(ledArray[i], i))
 
-def startupCheck():
+
+def startup_check():
     # Will turn on each group of LEDs to white and then off
-    for cell in cellArray:
+    for cell in cell_array:
         cell.led.on()
         sleep(0.5)
-    for cell in cellArray:
+    for cell in cell_array:
         cell.led.off()
-        
+
+
 def cleanup():
     # turns off LEDs and resets Cells to healthy
-    for cell in cellArray:
+    for cell in cell_array:
         cell.led.off()
         cell.status = "healthy"
 
-print("Starting Vaccine Exhibit...")
-startupCheck()
 
-debug = False    #set to True to debug
+print("Starting Vaccine Exhibit...")
+startup_check()
+
+debug = False  # set to debug level
 
 while True:
     if button.is_pressed:
-        for i, cell in enumerate(cellArray):
-            cell.updateStatus(sensorArray[i], debug)
+        for i, cell in enumerate(cell_array):
+            cell.update_status(sensor_array[i], debug)
         sleep(1) if debug else None
     else:
         cleanup()
